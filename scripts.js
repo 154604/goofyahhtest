ad70138d9a15b160ca7de164fbb7ee9a94287998
Arya <154604@mtsd.org>
2024-10-22 08:22:24 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,3 +15,19 @@ function index4() {
 function access() {
   window.location.href = "/access.html";
 }
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
